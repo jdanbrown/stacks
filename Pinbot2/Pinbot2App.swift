3d6354305bb669b91f24b5802f01be1788653df3
Dan Brown <jdanbrown@gmail.com>
2021-12-03 23:18:51 -0700

    xcode setup

---
+++ Pinbot2/Pinbot2App.swift
@@ -0,0 +1,17 @@
+//
+//  Pinbot2App.swift
+//  Pinbot2
+//
+//  Created by Dan Brown on 2021-12-03.
+//
+
+import SwiftUI
+
+@main
+struct Pinbot2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pinbot2UITests/Pinbot2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Pinbot2UITestsLaunchTests.swift
+//  Pinbot2UITests
+//
+//  Created by Dan Brown on 2021-12-03.
+//
+
+import XCTest
+
+class Pinbot2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
